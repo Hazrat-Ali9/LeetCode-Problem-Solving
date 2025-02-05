271957c80244aa3ee5fe937d28b02eb2a55eeabd
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-02-05 10:47:39 +0600

    Valid Anagram  😈

---
+++ valid anagram.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if s==t: return True
+        if len(s)!=len(t): return False
+        v = [0]*26
+        for i in range(len(s)):
+            v[ord(s[i])-ord('a')] += 1
+            v[ord(t[i])-ord('a')] -= 1
+        for i in range(26):
+            if v[i]: return False
+        return True
+
+        
