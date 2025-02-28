65bc570dc50c5885e25aa3679d6a0f88306c31e3
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-03-01 05:56:44 +0600

    missing number  🐘

---
+++ missing-number.js
@@ -0,0 +1,16 @@
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const missingNumber = nums => {
+  let res = nums.length;
+
+  for (i = 0; i < nums.length; i++) {
+    res ^= i ^ nums[i]; // a ^ b ^ b = a
+  }
+
+  return res;
+};
+
+export { missingNumber };
