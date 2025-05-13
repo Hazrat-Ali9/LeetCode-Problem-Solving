caf1aafd5a479b5b60f242741f4d734fad2cf304
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-05-13 09:00:26 +0600

    gray code 🫣

---
+++ gray-code.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} n
+ * @return {number[]}
+ */
+const grayCode = n => {
+  const result = [];
+
+  for (let i = 0; i < 1 << n; i++) {
+    result.push(i ^ (i >> 1));
+  }
+
+  return result;
+};
+
+
