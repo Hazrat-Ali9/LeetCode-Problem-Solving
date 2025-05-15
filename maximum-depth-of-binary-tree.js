38ba999faa7d177a08d0d9ac39298ea5b2d9c938
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-05-15 09:04:37 +0600

    Maximum depth of binary tree  🥶

---
+++ maximum-depth-of-binary-tree.js
@@ -0,0 +1,13 @@
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+const maxDepth = root => {
+  if (!root) {
+    return 0;
+  }
+
+  return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+};
+
+
