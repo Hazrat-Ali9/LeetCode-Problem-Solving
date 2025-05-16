32cb3eef5a435bc365d2bc3f480dc1f1373f9c1b
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-05-16 07:08:20 +0600

    Path Sum :bug:

---
+++ path-sum.js
@@ -0,0 +1,18 @@
+/**
+ * @param {TreeNode} root
+ * @param {number} sum
+ * @return {boolean}
+ */
+const hasPathSum = (root, sum) => {
+  if (!root) {
+    return false;
+  }
+
+  if (!root.left && !root.right) {
+    return root.val === sum;
+  }
+
+  return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
+};
+
+
