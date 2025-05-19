9e4588d19910d981ee0ab5fe892a0142db96e736
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-05-19 08:46:45 +0600

    Minimum Depth of Binary Tree  🪀

---
+++ minimum-depth-of-binary-tree.js
@@ -0,0 +1,21 @@
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+const minDepth = root => {
+  if (!root) {
+    return 0;
+  }
+
+  if (!root.left) {
+    return 1 + minDepth(root.right);
+  }
+
+  if (!root.right) {
+    return 1 + minDepth(root.left);
+  }
+
+  return 1 + Math.min(minDepth(root.left), minDepth(root.right));
+};
+
+
