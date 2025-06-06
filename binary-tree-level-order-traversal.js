4652ec153d2d799ada26bb6cb880263288efe0eb
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-06-06 10:07:21 +0600

    Binary tree level order traversal  🐸

---
+++ binary-tree-level-order-traversal.js
@@ -0,0 +1,40 @@
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+const levelOrder = root => {
+  if (!root) {
+    return [];
+  }
+
+  const result = [];
+  const queue = [root, null];
+  let level = [];
+
+  while (queue.length > 0) {
+    const node = queue.shift();
+
+    if (node) {
+      level.push(node.val);
+
+      if (node.left) {
+        queue.push(node.left);
+      }
+
+      if (node.right) {
+        queue.push(node.right);
+      }
+    } else {
+      result.push(level);
+
+      if (queue.length > 0) {
+        level = [];
+        queue.push(null);
+      }
+    }
+  }
+
+  return result;
+};
+
+
