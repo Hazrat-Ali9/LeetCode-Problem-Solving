cd7a86fc721f3499f8eafd65c02f5ef6c033e03c
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-06-07 19:41:39 +0600

    Second Minimum node in a binary tree  🤮

---
+++ second-minimum-node-in-a-binary-tree.js
@@ -0,0 +1,36 @@
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var findSecondMinimumValue = function(root) {
+  if (!root) {
+    return -1;
+  }
+
+  if (!root.left && !root.right) {
+    return -1;
+  }
+
+  var left = root.left.val;
+  var right = root.right.val;
+
+  if (left === root.val) {
+    left = findSecondMinimumValue(root.left);
+  }
+
+  if (right === root.val) {
+    right = findSecondMinimumValue(root.right);
+  }
+
+  if (left !== -1 && right !== -1) {
+    return Math.min(left, right);
+  }
+
+  if (left !== -1) {
+    return left;
+  }
+
+  return right;
+};
+
+
