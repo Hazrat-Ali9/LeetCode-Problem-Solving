121c9670d46797f5f54895c80f97e82e0078d127
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-07-03 10:01:52 +0600

    Lowest common ancestor of a binary search tree 🛸

---
+++ lowest-common-ancestor-of-a-binary-search-tree.js
@@ -0,0 +1,23 @@
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+const lowestCommonAncestor = (root, p, q) => {
+  if (!root) {
+    return null;
+  }
+
+  if (root.val > p.val && root.val > q.val) {
+    return lowestCommonAncestor(root.left, p, q);
+  }
+
+  if (root.val < p.val && root.val < q.val) {
+    return lowestCommonAncestor(root.right, p, q);
+  }
+
+  return root;
+};
+
+
