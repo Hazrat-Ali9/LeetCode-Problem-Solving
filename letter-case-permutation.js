bb503ad4883a3c81d0930eeb2ccd51fb08d40dbd
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-07-09 07:39:53 +0600

    Letter case Permutation  🚒

---
+++ letter-case-permutation.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} S
+ * @return {string[]}
+ */
+const letterCasePermutation = S => {
+  const result = [];
+  backtracking(S, 0, '', result);
+  return result;
+};
+
+const backtracking = (S, i, solution, result) => {
+  if (i === S.length) {
+    result.push(solution);
+    return;
+  }
+
+  backtracking(S, i + 1, solution + S[i].toLowerCase(), result);
+
+  if (/[a-zA-Z]/.test(S[i])) {
+    backtracking(S, i + 1, solution + S[i].toUpperCase(), result);
+  }
+};
+
