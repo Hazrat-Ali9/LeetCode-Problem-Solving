607ff7a210d53839a0b75fde50d1aa3514f8260b
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-07-11 00:28:54 +0600

    Linked list components   ✈

---
+++ linked-list-components.js
@@ -0,0 +1,27 @@
+/**
+ * @param {ListNode} head
+ * @param {number[]} G
+ * @return {number}
+ */
+const numComponents = (head, G) => {
+  const set = new Set(G);
+
+  let count = 0;
+  let node = head;
+
+  while (node) {
+    if (set.has(node.val)) {
+      count++;
+
+      while (node && set.has(node.val)) {
+        node = node.next;
+      }
+    } else {
+      node = node.next;
+    }
+  }
+
+  return count;
+};
+
+
