ce0bae1645521edb3565c0144e83278850f3199e
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-07-24 07:29:01 +0600

    German book b1 answe 🏣

---
+++ sort-characters-by-frequency.js
@@ -0,0 +1,28 @@
+/**
+ *
+ * @param {string} s
+ * @return {string}
+ */
+const frequencySort = s => {
+  
+  const map = {};
+  for (let c of s) {
+    map[c] = ~~map[c] + 1;
+  }
+
+  
+  const bucket = Array(s.length + 1).fill('');
+  Object.keys(map).forEach(c => {
+    const count = map[c];
+    bucket[count] += c.repeat(count);
+  });
+
+  
+  let result = '';
+  for (let i = bucket.length - 1; i > 0; i--) {
+    result += bucket[i];
+  }
+  return result;
+};
+
+
