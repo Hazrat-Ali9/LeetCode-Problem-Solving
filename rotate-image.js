0cdeee34f7ec32faaf1cbce6b762b20977861b98
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-07-28 10:16:42 +0600

    Rotate image :bulb:

---
+++ rotate-image.js
@@ -0,0 +1,11 @@
+const rotate = matrix => {
+  matrix.reverse();
+
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = i + 1; j < matrix[i].length; j++) {
+      [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]];
+    }
+  }
+};
+
+
