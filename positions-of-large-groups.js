cc332dadfec710a65ceeddaea837cbb431bd8287
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-09-05 10:02:04 +0600

    Positions of large groups  🤯

---
+++ positions-of-large-groups.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} S
+ * @return {number[][]}
+ */
+const largeGroupPositions = S => {
+  const result = [];
+
+  let i = 0;
+  let j = 0;
+
+  while (j < S.length) {
+    while (j < S.length && S[i] === S[j]) {
+      j++;
+    }
+
+    const len = j - i;
+
+    if (len >= 3) {
+      result.push([i, j - 1]);
+    }
+
+    i = j;
+  }
+
+  return result;
+};
