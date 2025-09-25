57518462635e7f3a6903f9b426a34fc2b3ce5494
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-09-25 09:30:16 +0600

    Remove Duplicates from Sorted List II :books:

---
+++ remove-duplicates-from-sorted-list-ii.js
@@ -0,0 +1,33 @@
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+const deleteDuplicates = head => {
+  const dummy = new ListNode(0);
+
+  let p = dummy;
+  let count = 0;
+  let slow = head;
+  let fast = head;
+
+  while (fast) {
+    while (fast && fast.val === slow.val) {
+      fast = fast.next;
+      count++;
+    }
+
+    if (count === 1) {
+      p.next = slow;
+      p = p.next;
+    }
+
+    slow = fast;
+    count = 0;
+  }
+
+  if (p) {
+    p.next = null;
+  }
+
+  return dummy.next;
+};
