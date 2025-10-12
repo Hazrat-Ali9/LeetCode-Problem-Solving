1c5fff8d8aa9e06fd879ac0ef046b2d6e501604b
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-10-12 08:42:21 +0600

    palindrome number :tada:

---
+++ palindrome-number.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    
+    if (x < 0) return false;
+    let str = x.toString();
+
+    let reversed = str.split('').reverse().join('');
+
+    return str === reversed;
+};
+console.log(isPalindrome(121));   
+console.log(isPalindrome(-121));  
+console.log(isPalindrome(10));    
+
