5fd9d0281746b94300b2a43cdb0728f68e7f5a8e
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-10-17 10:40:54 +0600

    Excel Sheet Column Number  :rocket:

---
+++ excel_sheet_column_number.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} columnTitle
+ * @return {number}
+ */
+var titleToNumber = function(columnTitle) {
+    let result = 0;
+    for (let i = 0; i < columnTitle.length; i++) {
+        let value = columnTitle.charCodeAt(i) - 'A'.charCodeAt(0) + 1; 
+        result = result * 26 + value;
+    }
+    return result;
+};
+
+console.log(titleToNumber("A"));   
+console.log(titleToNumber("AB"));  
+console.log(titleToNumber("ZY"));  
+
