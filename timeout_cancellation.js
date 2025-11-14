0761007ccfe5e12b6ebc82d7148fd2afefa7c227
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-11-14 12:19:20 +0600

    Timeout Cancellation :books:

---
+++ timeout_cancellation.js
@@ -0,0 +1,10 @@
+var cancellable = function (fn, args, t) {
+  const timerId = setTimeout(() => {
+    fn(...args);
+  }, t);
+ 
+  return function cancelFn() {
+    clearTimeout(timerId); 
+  };
+};
+
