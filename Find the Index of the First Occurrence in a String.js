3b0bda03e7d033b0a5b9afd0f1237c5444cef053
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-12-12 12:29:09 +0600

    Find the Index of the First Occurrence in a String :tada:

---
+++ Find the Index of the First Occurrence in a String.js
@@ -0,0 +1,22 @@
+function strStr(haystack, needle) {
+    let n = haystack.length;
+    let m = needle.length;
+
+    if (m === 0) return 0;
+
+    for (let i = 0; i <= n - m; i++) {
+        let found = true;
+
+        for (let j = 0; j < m; j++) {
+            if (haystack[i + j] !== needle[j]) {
+                found = false;
+                break;
+            }
+        }
+
+        if (found) return i;
+    }
+
+    return -1;
+}
+
