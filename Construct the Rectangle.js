34f290883911b04b198c16e9ec94841c0be7eaa7
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-12-22 08:27:01 +0600

    Construct the Rectangle  🛫

---
+++ Construct the Rectangle.js
@@ -0,0 +1,11 @@
+var constructRectangle = function(area) {
+    let w = Math.floor(Math.sqrt(area));
+    
+    while (area % w !== 0) {
+        w--;
+    }
+    
+    let l = area / w;
+    return [l, w];
+};
+
