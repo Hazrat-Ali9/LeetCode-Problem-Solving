3dd2554f246392a3c71e842ebf492c12149d3de4
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-12-28 09:28:20 +0600

    Move Zeroes 🕌

---
+++ Move Zeroes.js
@@ -0,0 +1,15 @@
+var moveZeroes = function(nums) {
+    let pos = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== 0) {
+            nums[pos] = nums[i];
+            pos++;
+        }
+    }
+
+    while (pos < nums.length) {
+        nums[pos] = 0;
+        pos++;
+    }
+};
