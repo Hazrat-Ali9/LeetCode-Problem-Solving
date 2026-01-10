f1540840945eb05df5bb8d0a951321b0a8de7a72
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2026-01-10 11:38:41 +0600

    Longest Harmonious Subsequence  🚞 ☂

---
+++ Longest Harmonious Subsequence.js
@@ -0,0 +1,18 @@
+var findLHS = function(nums) {
+    const freq = new Map();
+
+    for (let num of nums) {
+        freq.set(num, (freq.get(num) || 0) + 1);
+    }
+
+    let maxLen = 0;
+   
+    for (let [num, count] of freq.entries()) {
+        if (freq.has(num + 1)) {
+            maxLen = Math.max(maxLen, count + freq.get(num + 1));
+        }
+    }
+
+    return maxLen;
+};
+
