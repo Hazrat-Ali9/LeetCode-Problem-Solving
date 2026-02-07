2d160d936743e990ad02befc315c014bf6705deb
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2026-02-07 06:55:57 +0600

    Decompress Run-Length Encoded List 🏯

---
+++ Decompress Run-Length Encoded List.js
@@ -0,0 +1,16 @@
+var decompressRLElist = function(nums) {
+    let result = [];
+
+    for (let i = 0; i < nums.length; i += 2) {
+        let freq = nums[i];
+        let val = nums[i + 1];
+
+        for (let j = 0; j < freq; j++) {
+            result.push(val);
+        }
+    }
+
+    return result;
+};
+
+
