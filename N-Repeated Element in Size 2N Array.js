75de81e3a4ccdfa79dce1d6dc1c7204a56da3f69
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2026-02-11 09:44:22 +0600

    N-Repeated Element in Size 2N Array :books:

---
+++ N-Repeated Element in Size 2N Array.js
@@ -0,0 +1,11 @@
+var repeatedNTimes = function(nums) {
+    let seen = new Set();
+
+    for (let num of nums) {
+        if (seen.has(num)) {
+            return num;  
+        }
+        seen.add(num);
+    }
+};
+
