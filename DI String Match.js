8d728d62a09256ddc894442038e7c27e55b896b8
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2026-02-14 10:29:31 +0600

    :tada:  DI String Match

---
+++ DI String Match.js
@@ -0,0 +1,20 @@
+var diStringMatch = function(s) {
+    let n = s.length;
+    let low = 0;
+    let high = n;
+    let result = [];
+
+    for (let i = 0; i < n; i++) {
+        if (s[i] === 'I') {
+            result.push(low);
+            low++;
+        } else { 
+            result.push(high);
+            high--;
+        }
+    }
+    result.push(low);  
+
+    return result;
+};
+
