c060fe8a07196c1e8004a485132865d9f5c65bb9
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2026-02-16 14:33:02 +0600

    :tada:   Squares of a Sorted Array

---
+++ Squares of a Sorted Array.js
@@ -0,0 +1,25 @@
+var sortedSquares = function(nums) {
+    let n = nums.length;
+    let result = new Array(n);
+    
+    let left = 0;
+    let right = n - 1;
+    let index = n - 1;
+
+    while (left <= right) {
+        let leftSquare = nums[left] * nums[left];
+        let rightSquare = nums[right] * nums[right];
+
+        if (leftSquare > rightSquare) {
+            result[index] = leftSquare;
+            left++;
+        } else {
+            result[index] = rightSquare;
+            right--;
+        }
+        index--;
+    }
+
+    return result;
+};
+
