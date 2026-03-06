c60e468e7dcf77c15ae83abe66f307f2fe7aff34
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2026-03-06 10:43:37 +0600

    Largest Divisible Subset   🤪

---
+++ Largest Divisible Subset.js
@@ -0,0 +1,32 @@
+var largestDivisibleSubset = function(nums) {
+    nums.sort((a, b) => a - b);
+
+    const n = nums.length;
+    const dp = new Array(n).fill(1);
+    const prev = new Array(n).fill(-1);
+
+    let maxSize = 1;
+    let maxIndex = 0;
+
+    for (let i = 1; i < n; i++) {
+        for (let j = 0; j < i; j++) {
+            if (nums[i] % nums[j] === 0 && dp[j] + 1 > dp[i]) {
+                dp[i] = dp[j] + 1;
+                prev[i] = j;
+            }
+        }
+
+        if (dp[i] > maxSize) {
+            maxSize = dp[i];
+            maxIndex = i;
+        }
+    }
+
+    const result = [];
+    while (maxIndex !== -1) {
+        result.push(nums[maxIndex]);
+        maxIndex = prev[maxIndex];
+    }
+
+    return result.reverse();
+};
