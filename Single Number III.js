b2b5011afa49bdc4028273365d091f410213d8f1
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2026-03-15 11:09:26 +0600

    Single Number III :rocket:

---
+++ Single Number III.js
@@ -0,0 +1,21 @@
+var singleNumber = function(nums) {
+    let xor = 0;
+
+    for (let num of nums) {
+        xor ^= num;
+    }
+
+    let diff = xor & -xor;
+
+    let a = 0, b = 0;
+
+    for (let num of nums) {
+        if ((num & diff) === 0) {
+            a ^= num;
+        } else {
+            b ^= num;
+        }
+    }
+
+    return [a, b];
+};
