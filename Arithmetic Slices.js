55bf8f1cfdaaec3c5ac0b6ab1f222017a0bacfb0
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2026-04-08 21:19:42 +0600

    Arithmetic Slices :rocket:

---
+++ Arithmetic Slices.js
@@ -0,0 +1,17 @@
+var numberOfArithmeticSlices = function(nums) {
+    if (nums.length < 3) return 0;
+
+    let total = 0;
+    let curr = 0;
+
+    for (let i = 2; i < nums.length; i++) {
+        if (nums[i] - nums[i - 1] === nums[i - 1] - nums[i - 2]) {
+            curr += 1;
+            total += curr;
+        } else {
+            curr = 0;
+        }
+    }
+
+    return total;
+};
