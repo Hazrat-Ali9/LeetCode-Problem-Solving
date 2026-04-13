6141e732c2fe418f3a2d7e9a1587dd4abeeb733b
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2026-04-13 09:13:47 +0600

    Minimum Moves to Equal Array Elements :books:

---
+++ Minimum Moves to Equal Array Elements.js
@@ -0,0 +1,10 @@
+var minMoves = function(nums) {
+    let minVal = Math.min(...nums);
+    let moves = 0;
+
+    for (let num of nums) {
+        moves += num - minVal;
+    }
+
+    return moves;
+};
