824571e703127de8b86a70a818fba03a5b025764
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2026-05-02 09:27:47 +0600

    Remove K Digits :rocket:

---
+++ Remove K Digits.js
@@ -0,0 +1,20 @@
+var removeKdigits = function(num, k) {
+    let stack = [];
+
+    for (let digit of num) {
+        while (stack.length && k > 0 && stack[stack.length - 1] > digit) {
+            stack.pop();
+            k--;
+        }
+        stack.push(digit);
+    }
+
+    while (k > 0) {
+        stack.pop();
+        k--;
+    }
+
+    let result = stack.join('').replace(/^0+/, '');
+
+    return result === '' ? '0' : result;
+};
