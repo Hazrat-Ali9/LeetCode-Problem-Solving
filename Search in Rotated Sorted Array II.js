f41eb6d2a794b95a9208d0cc4ec9ae0866c566d9
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2026-05-12 09:28:19 +0600

    Unique Binary Search Trees II 🚠

--- Search in Rotated Sorted Array II.js
+++ Search in Rotated Sorted Array II.js
@@ -1,1 +1,35 @@
+var generateTrees = function(n) {
 
+    if (n === 0) return [];
+
+    function build(start, end) {
+
+        let result = [];
+        if (start > end) {
+            result.push(null);
+            return result;
+        }
+
+        for (let i = start; i <= end; i++) {
+
+            let leftTrees = build(start, i - 1);
+            let rightTrees = build(i + 1, end);
+
+            for (let left of leftTrees) {
+                for (let right of rightTrees) {
+
+                    let root = new TreeNode(i);
+
+                    root.left = left;
+                    root.right = right;
+
+                    result.push(root);
+                }
+            }
+        }
+
+        return result;
+    }
+
+    return build(1, n);
+};
