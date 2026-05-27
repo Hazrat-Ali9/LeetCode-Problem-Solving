b089cbe7d63e301293915f336a40cce413a5761b
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2026-05-27 10:06:56 +0600

    Longest Palindromic Subsequence :tada:

---
+++ Longest Palindromic Subsequence.js
@@ -0,0 +1,38 @@
+var longestPalindromeSubseq = function(s) {
+
+    let n = s.length;
+
+    let dp = Array.from({ length: n }, () =>
+        Array(n).fill(0)
+    );
+
+    for (let i = 0; i < n; i++) {
+        dp[i][i] = 1;
+    }
+
+    for (let len = 2; len <= n; len++) {
+
+        for (let i = 0; i <= n - len; i++) {
+
+            let j = i + len - 1;
+
+            if (s[i] === s[j]) {
+
+                if (len === 2) {
+                    dp[i][j] = 2;
+                } else {
+                    dp[i][j] = dp[i + 1][j - 1] + 2;
+                }
+
+            } else {
+
+                dp[i][j] = Math.max(
+                    dp[i + 1][j],
+                    dp[i][j - 1]
+                );
+            }
+        }
+    }
+
+    return dp[0][n - 1];
+};
