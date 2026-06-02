b78c4f4dfc92533e8068c7aebb592cde1c3bb3b5
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2026-06-02 09:27:59 +0600

    Guess Number Higher or Lower  🔰

---
+++ Guess Number Higher or Lower.js
@@ -0,0 +1,19 @@
+var guessNumber = function(n) {
+    let left = 1;
+    let right = n;
+
+    while (left <= right) {
+        const mid = left + Math.floor((right - left) / 2);
+        const res = guess(mid);
+
+        if (res === 0) {
+            return mid;
+        } else if (res === -1) {
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        }
+    }
+
+    return -1;
+};
