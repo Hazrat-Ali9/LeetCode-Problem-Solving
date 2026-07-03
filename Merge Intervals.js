ae8156529e86dde815c9e9f1abd3ae751df17ad8
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2026-07-03 08:54:43 +0600

    Perfect Number 🥸

--- Merge Intervals.js
+++ Merge Intervals.js
@@ -1,1 +1,26 @@
+/**
+ * @param {number[][]} intervals
+ * @return {number[][]}
+ */
+var merge = function(intervals) {
+    if (intervals.length === 0) return [];
 
+    intervals.sort((a, b) => a[0] - b[0]);
+
+    const result = [];
+    let current = intervals[0];
+
+    for (let i = 1; i < intervals.length; i++) {
+        if (intervals[i][0] <= current[1]) {
+            
+            current[1] = Math.max(current[1], intervals[i][1]);
+        } else {
+            result.push(current);
+            current = intervals[i];
+        }
+    }
+
+    result.push(current);
+
+    return result;
+};
