e1606a5d7e5d3047316c6ba347e8b00e046eaa5e
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2026-07-10 09:37:51 +0600

    Bitwise ORs of Subarrays :tada:

---
+++ Bitwise ORs of Subarrays.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+var subarrayBitwiseORs = function(arr) {
+    let result = new Set();
+    let prev = new Set();
+
+    for (let num of arr) {
+        let curr = new Set();
+        curr.add(num);
+
+        for (let val of prev) {
+            curr.add(val | num);
+        }
+
+        for (let val of curr) {
+            result.add(val);
+        }
+
+        prev = curr;
+    }
+
+    return result.size;
+};
