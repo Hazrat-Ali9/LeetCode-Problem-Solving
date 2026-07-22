16f48380b9a058bb91933600514d81716d513fae
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2026-07-22 09:02:40 +0600

    Invert Binary Tree  🤢

---
+++ Invert Binary Tree.js
@@ -0,0 +1,13 @@
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var invertTree = function(root) {
+    if (root === null) return null;
+
+    const temp = root.left;
+    root.left = invertTree(root.right);
+    root.right = invertTree(temp);
+
+    return root;
+};
--- Maximum Sum of Two Non-Overlapping Subarrays.js
+++ Maximum Sum of Two Non-Overlapping Subarrays.js
@@ -1,1 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @param {number} firstLen
+ * @param {number} secondLen
+ * @return {number}
+ */
+var maxSumTwoNoOverlap = function(nums, firstLen, secondLen) {
+    const n = nums.length;
+    const prefix = new Array(n + 1).fill(0);
 
+    for (let i = 0; i < n; i++) {
+        prefix[i + 1] = prefix[i] + nums[i];
+    }
+
+    function getMax(L, M) {
+        let maxL = prefix[L] - prefix[0];
+        let ans = 0;
+
+        for (let i = L + M; i <= n; i++) {
+            maxL = Math.max(maxL, prefix[i - M] - prefix[i - M - L]);
+            ans = Math.max(ans, maxL + (prefix[i] - prefix[i - M]));
+        }
+
+        return ans;
+    }
+
+    return Math.max(getMax(firstLen, secondLen), getMax(secondLen, firstLen));
+};
