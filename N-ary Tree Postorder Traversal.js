5a57580c28f655874d4921753412af20e07c4839
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2026-08-04 07:59:12 +0600

    N-ary Tree Postorder Traversal 🚂

---
+++ N-ary Tree Postorder Traversal.js
@@ -0,0 +1,28 @@
+/**
+ * // Definition for a Node.
+ * function Node(val, children) {
+ *    this.val = val;
+ *    this.children = children;
+ * };
+ */
+
+/**
+ * @param {Node|null} root
+ * @return {number[]}
+ */
+var postorder = function(root) {
+    const result = [];
+
+    function dfs(node) {
+        if (!node) return;
+
+        for (const child of node.children) {
+            dfs(child);
+        }
+
+        result.push(node.val);
+    }
+
+    dfs(root);
+    return result;
+};
