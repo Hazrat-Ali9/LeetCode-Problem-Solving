59e78659283a8003ddb4e0dfc59e18e98d16ed60
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2026-08-18 22:41:55 +0600

    Minimum Absolute Difference in BST :tada:

---
+++ Minimum Absolute Difference in BST.js
@@ -0,0 +1,25 @@
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var getMinimumDifference = function(root) {
+    let prev = null;
+    let minDiff = Infinity;
+
+    function inorder(node) {
+        if (!node) return;
+
+        inorder(node.left);
+
+        if (prev !== null) {
+            minDiff = Math.min(minDiff, node.val - prev);
+        }
+
+        prev = node.val;
+        inorder(node.right);
+    }
+
+    inorder(root);
+
+    return minDiff;
+};
