a2babd09d00c93f7e5172a0e75f964c94dab40fb
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2026-09-17 12:52:33 +0600

    Rank Transform of an Array :rocket:

---
+++ Rank Transform of an Array.js
@@ -0,0 +1,15 @@
+var arrayRankTransform = function(arr) {
+    const sorted = [...arr].sort((a, b) => a - b);
+
+    const rank = new Map();
+    let currentRank = 1;
+
+    for (const num of sorted) {
+        if (!rank.has(num)) {
+            rank.set(num, currentRank);
+            currentRank++;
+        }
+    }
+
+    return arr.map(num => rank.get(num));
+};
